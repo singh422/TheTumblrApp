2775be552de76f41406b208c361b01d3a86abb52
Avinash <@purdue.edu>
2017-11-17 17:25:29 -0500

    First Commit

---
+++ Tumblr/PhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosTableViewCell.swift
+//  Tumblr
+//
+//  Created by Avinash Singh on 11/11/17.
+//  Copyright © 2017 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
